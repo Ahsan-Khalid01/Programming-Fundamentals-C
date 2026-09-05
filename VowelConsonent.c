d5311736aa45a1b80599fc87cdb5d55a84732bf4
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-09-05 22:49:41 +0500

    Add files via upload

---
+++ VowelConsonent.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+	char ch;
+	printf("Enter a character....");
+	scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+}
+
+	if(ch>='a' && ch<='z')
+	{
+	
+printf("Lower case Vowel");
+
+}
+else
+{
+	printf("lower case consonent");
+}
+	
+if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+	}
+		if(ch>='A' && ch<='Z')
+		{
+			printf("upper case vowel");
+		}
+		else 
+		{
+			printf("upper case consonent");
+		}
+	
+	return 0;
+}
