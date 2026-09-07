b090dcb0d87195308697dbea6c51326a7549df9e
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-09-08 01:26:10 +0500

    Add files via upload

---
+++ stndalone if ,c.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+	int num1,num2;
+	printf("enter a two number.......");
+	scanf("%d%d",&num1,&num2);
+	if(num1>num2){
+		printf("%d is a greater    \n",num1);
+		
+	}
+if 
+(num2>num1);
+	{
+		printf("%d is a greater    \n",num2);
+		
+	}
+	else
+	
+		printf(" both number are equal ",num1,num2);
+	}
+	return 0;
+}
