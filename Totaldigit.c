51b7fb524a3d72c0c478e26176740ca831d9a100
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-09-26 21:17:01 +0500

    Add files via upload

---
+++ Totaldigit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,i=0;
+	printf("enter a number........");
+	scanf("%d",&n);
+	while(n>0){
+		n=n/10;
+		i++;
+		
+	}
+	printf("Total digit is %d",i);
+	return 0;
+
+}
